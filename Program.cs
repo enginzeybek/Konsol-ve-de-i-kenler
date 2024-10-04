03b7fe4db2a7d4537156024941065ca9628cf6de
enginzeybek <135993110+enginzeybek@users.noreply.github.com>
2024-10-04 20:08:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Merhaba");
+Console.WriteLine("-----------");
+Console.WriteLine("Engin Zeybek");
+Console.Write("--------------------");
+Console.WriteLine("İsminizi Girin");
+string? name = Console.ReadLine();
+Console.WriteLine("Soyadınız");
+string? surname = Console.ReadLine();
+Console.WriteLine("Ad:" + name + " " + "Soyad:" + surname);
+
+string greeting = "Merhaba";
+int number = 4;
+bool question = true;
+decimal ondalik = 51.5m;
+DateTime tarih = DateTime.Now;
+char harf = 'A';
